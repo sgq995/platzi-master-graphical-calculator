2056aee71ba7b5895083c75c508d1c1fda6c54da
Sebastian G. Q <sgq995@gmail.com>
2021-10-17 14:29:10 -0500

    feat(render): Enhance renderer usage

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,4 +1,4 @@
-import { isArray, isNumber, isString } from "lodash";
+import { flattenDeep, isArray, isNumber, isString } from "lodash";
 
 /**
  * 
@@ -8,8 +8,13 @@ import { isArray, isNumber, isString } from "lodash";
  */
 export function render(element, container) {
   if (isArray(element)) {
-    element.forEach(child => container.appendChild(child));
-  } else if (element instanceof HTMLElement) {
+    flattenDeep(element)
+      .forEach(child => {
+        if (child instanceof Node) {
+          container.appendChild(child)
+        }
+      });
+  } else if (element instanceof Node) {
     container.appendChild(element);
   } else if (isString(element) || isNumber(element)) {
     container.textContent = element;
@@ -23,7 +28,12 @@ export function render(element, container) {
  */
 export function renderSVG(element, container) {
   if (isArray(element)) {
-    element.forEach(child => container.appendChild(child));
+    flattenDeep(element)
+      .forEach(child => {
+        if (child instanceof SVGElement) {
+          container.appendChild(child)
+        }
+      });
   } else if (element instanceof SVGElement) {
     container.appendChild(element);
   }
