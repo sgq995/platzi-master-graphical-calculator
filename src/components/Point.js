17a9693fb30ffe2aa515e748594a32989771b50f
Sebastian G. Q <sgq995@gmail.com>
2021-10-17 11:10:59 -0500

    feat(point): Inject methods to handle plane transformations

--- src/components/Point.js
+++ src/components/Point.js
@@ -3,10 +3,10 @@ function Point({ cx, cy, fill }) {
   const BIG_RADIUS = 5;
 
   const point = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  point.setAttribute('cx', 320);
-  point.setAttribute('cy', 240);
   point.setAttribute('r', SMALL_RADIUS);
   point.setAttribute('fill', 'black');
+  point.setAttribute('cx', cx);
+  point.setAttribute('cy', cy);
 
   point.addEventListener('mouseenter', function () {
     point.setAttribute('r', BIG_RADIUS);
@@ -15,6 +15,38 @@ function Point({ cx, cy, fill }) {
   point.addEventListener('mouseleave', function () {
     point.setAttribute('r', SMALL_RADIUS);
   });
+
+  const setAttribute = point.setAttribute.bind(point);
+  point.setAttribute = function (qualifiedName, value) {
+    console.log({ qualifiedName, value });
+    switch (qualifiedName) {
+      case 'cx':
+        setAttribute(qualifiedName, value);
+        break;
+
+      case 'cy':
+        setAttribute(qualifiedName, value);
+        break;
+
+      default: point.setAttribute('cx', cx);
+        setAttribute(qualifiedName, value);
+        break;
+    }
+  }
+
+  const getAttribute = point.getAttribute.bind(point);
+  point.getAttribute = function (qualifiedName) {
+    switch (qualifiedName) {
+      case 'cx':
+        return getAttribute(qualifiedName);
+
+      case 'cy':
+        return getAttribute(qualifiedName);
+
+      default:
+        return getAttribute(qualifiedName);
+    }
+  }
 
   return point;
 }
